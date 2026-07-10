97314ec42d24914bf78776063d87c0e27798b181
saalxhh007 <153205487+saalxhh007@users.noreply.github.com>
2026-07-10 18:57:10 +0100

    updated db config
    Updated database connection to use DATABASE_URL with SSL options.

--- config/db.js
+++ config/db.js
@@ -2,23 +2,16 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config()
-
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
-    port: process.env.DB_PORT || 5432,
-    logging: false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+  process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
+  },
+}
 )
 
 export default db
